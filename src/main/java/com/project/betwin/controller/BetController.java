43120b1a8ec02b6c32f3de80a306c45b4f066a66
diegoFranciscoo <diegofrancisco.contato@gmail.com>
2024-08-31 14:30:31 -0300

    adding documentation using swagger

--- src/main/java/com/project/betwin/controller/BetController.java
+++ src/main/java/com/project/betwin/controller/BetController.java
@@ -4,6 +4,9 @@ import com.project.betwin.domain.Bet;
 import com.project.betwin.dto.BetRequestDTO;
 import com.project.betwin.dto.BetResponseDTO;
 import com.project.betwin.service.impl.BetServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,13 +21,19 @@ import java.util.List;
 public class BetController {
     private final BetServiceImpl service;
 
+    @Operation(description = "Create a new bet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully create a bet"),
+            @ApiResponse(responseCode = "400", description = "Throws MinimalAmountException, InsufficientAmountException, or ErrorProcessingBetException based on the condition")
+    })
     @PostMapping
     public ResponseEntity<BetResponseDTO> createBet(@RequestBody @Valid BetRequestDTO bet) {
         return new ResponseEntity<>(service.createBet(bet), HttpStatus.CREATED);
     }
 
+    @Operation(description = "Returns all bets in the system")
     @GetMapping
-    public ResponseEntity<List<Bet>> getBet(){
+    public ResponseEntity<List<Bet>> getBet() {
         return new ResponseEntity<>(service.getBet(), HttpStatus.OK);
     }
 }
--- src/main/java/com/project/betwin/controller/UserController.java
+++ src/main/java/com/project/betwin/controller/UserController.java
@@ -3,6 +3,8 @@ package com.project.betwin.controller;
 import com.project.betwin.domain.User;
 import com.project.betwin.dto.UserDTO;
 import com.project.betwin.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,16 @@ import java.util.List;
 public class UserController {
     private final UserService service;
 
+
+    @Operation(description = "Create a new user")
+    @ApiResponse(responseCode = "201")
     @PostMapping(path = "/register")
     public ResponseEntity<User> createUser(@RequestBody @Valid UserDTO user) {
         return new ResponseEntity<>(service.createUser(user), HttpStatus.CREATED);
     }
 
+    @Operation(description = "Return all users in the system")
+    @ApiResponse(responseCode = "200")
     @GetMapping
     public ResponseEntity<List<User>> getUser() {
         return new ResponseEntity<>(service.getUser(), HttpStatus.OK);
